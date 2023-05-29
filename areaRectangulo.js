66bafa3ef9912fff9b4e529b6218e050f91dda6f
mariabelenarias <belenarias_25@hotmail.com>
2023-05-28 22:09:37 -0300

    Practico N0 finalizado

---
+++ areaRectangulo.js
@@ -0,0 +1,13 @@
+let base = 5;
+let altura = 3;
+let area = base*altura;
+console.log (area);
+
+// El àrea de un rectàngulo de base 5 cm y altura 3 cm es 15 cm.
+
+base = 12.5;
+altura = 5.3;
+area = base*altura;
+console.log (area);
+
+// El àrea de un rectàngulo de base 12.5 cm y altura 5.3 cm es 66.25.
