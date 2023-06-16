3e413ef3de5422b3aa7f2a9cc50e5803d65e3212
mariabelenarias <belenarias_25@hotmail.com>
2023-06-15 21:54:58 -0300

    agrego ejercicio 5

---
+++ practico1/frutas.js
@@ -0,0 +1,6 @@
+let frutas = [];
+frutas = ["manzana", "banana", "pera", "frutilla", "naranja"];
+let ultima = (frutas [4]);
+console.log ("La ultima fruta agregada fue: "+ ultima);
+let primera = (frutas [0]);
+console.log ("La primera fruta agregada fue: "+ primera);
