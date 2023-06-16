267c4434bd41f1700c544eaa1f753231c61eaa50
mariabelenarias <belenarias_25@hotmail.com>
2023-06-15 21:53:19 -0300

    agrego ejercicio 3

---
+++ practico1/operacionesAritmeticas.js
@@ -0,0 +1,9 @@
+let var1 = 1;
+let var2 = 10;
+let var3 = 6;
+let var4 = 2;
+let suma = var1 + var2;
+let resta = var3 - var4;
+let resultadoFinal = suma*resta;
+let esPar = ((resultadoFinal%2)==0);
+console.log ("Mis variables iniciales fueron: " + var1 + ", " + var2 + ", " + var3 + " y " + var4 + ". La respuesta a verificar si el resultado final es par es:" + esPar);
