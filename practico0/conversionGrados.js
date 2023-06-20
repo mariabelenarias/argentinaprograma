2e4e93aa2addd054e44d5e7d4ed1fa9908953744
mariabelenarias <belenarias_25@hotmail.com>
2023-06-20 00:57:06 -0300

    creo carpeta practico0

---
+++ practico0/conversionGrados.js
@@ -0,0 +1,11 @@
+let fahrenheit = 50;
+let celsius = (fahrenheit - 32)*(5/9);
+console.log (celsius);
+
+// 50 grados Fahrenheit son 10 grados Celsius.
+
+fahrenheit = 75;
+celsius = (fahrenheit - 32)*(5/9);
+console.log (celsius);
+
+// 75 grados Fahrenheit son 23.88 grados Celsius.
---
+++ practico0/promedio3.js
@@ -0,0 +1,23 @@
+let num1 = 5;
+let num2 = 7;
+let num3 = 9;
+let promedio = (num1+num2+num3)/3;
+console.log (promedio);
+
+// El promedio entre 5, 7 y 9 es 7.
+
+num1 = -2;
+num2 = -3;
+num3 = -8;
+promedio = (num1+num2+num3)/3;
+console.log (promedio);
+
+// El promedio entre -2, -3 y -8 es -4.33.
+
+num1 = -1.2;
+num2 = 5.3;
+num3 = -3.3;
+promedio = (num1+num2+num3)/3;
+console.log (promedio);
+
+// El promedio entre -1.2, 5.3 y -3.3 es 0,26.
