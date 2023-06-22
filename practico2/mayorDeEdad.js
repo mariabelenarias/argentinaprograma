c93d7aed360c547e8c28b158a808dbf350bc7171
mariabelenarias <belenarias_25@hotmail.com>
2023-06-22 20:38:52 -0300

    agrego ejercicio 1 a

---
+++ practico2/mayorDeEdad.js
@@ -0,0 +1,7 @@
+const readlineSync = require('readline-sync');
+let edad = readlineSync.question("Ingrese su edad: ");
+if(edad >= 18) {
+console.log ("Eres mayor de edad"); 
+}else{
+console.log ("Eres menor de edad"); 
+}
